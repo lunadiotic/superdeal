5314d4e4fe796ffd473362f33bb737e71c6813c9
aim <ahmadhakim404@gmail.com>
2024-01-10 20:43:40 +0700

    mapping data return with dto

--- src/AuctionService/Entities/Auction.cs
+++ src/AuctionService/Entities/Auction.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Http.Features;
-
 namespace AuctionService.Entities;
 
 public class Auction
@@ -8,10 +6,10 @@ public class Auction
     public int ReservePrice { get; set; } = 0;
     public string Seller { get; set; }
     public string Winner { get; set; }
-    public int? SoldAmount { get; set; } 
+    public int? SoldAmount { get; set; }
     public int? CurrentHighBid { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime UpdatedAt {get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     public DateTime AuctionEnd { get; set; }
     public Status Status { get; set; }
     public Item Item { get; set; }
