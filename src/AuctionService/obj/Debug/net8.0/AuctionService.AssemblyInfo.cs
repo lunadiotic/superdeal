0792bfab887a6131a118f29d58044bfc18a2dfb7
aim <ahmadhakim404@gmail.com>
2024-01-10 19:45:55 +0700

    build docker composer for postgress and add gitignore

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b63a485c542dc57754029c69b22558fa6527de6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb5b40a49c5db9152ebb6ccbc1813518bd29a523")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
