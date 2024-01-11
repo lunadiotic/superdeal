2cbc3a8e0634d7b7c11a9afa054daeac04d9378e
aim <ahmadhakim404@gmail.com>
2024-01-11 20:26:19 +0700

    add update auction api endpoint

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -48,4 +48,25 @@ public class AuctionsController : ControllerBase
 
         return CreatedAtAction(nameof(GetAuctionById), new { auction.Id }, _mapper.Map<AuctionDto>(auction));
     }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+    {
+        var auction = await _context.Auctions.Include(x => x.Item)
+                        .FirstOrDefaultAsync(x => x.Id == id);
+        if (auction == null) return NotFound();
+
+        // TODO: check if current user is the seller
+        
+        auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+        auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+        auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+        auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+        auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+
+        var result = await _context.SaveChangesAsync() > 0;
+        if (!result) return BadRequest("Failed to update auction");
+        return Ok();
+    }
+
 }
