d0377332d29997e515b9358ef50a51cfff28ad1f
aim <ahmadhakim404@gmail.com>
2024-01-12 22:57:25 +0700

    setup model and connection to mongodb in search service project

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,4 +12,16 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseAuthorization();
 app.MapControllers();
+
+// Connect to MongoDB and create indexes for search
+await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(
+        builder.Configuration.GetConnectionString("MongoDbConnection")
+    )
+);
+await DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
+
 app.Run();
